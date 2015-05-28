76c9dba2faf9452711e0127764a6513f78f839d2
Murad Yusufov <flood4life@gmail.com>
2015-05-28 19:21:12 +0300

    Add Logins controller with token-base authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::API
   respond_to :json
+
+  include AbstractController::Translation
+
+  before_action :authenticate_user_from_token!
+
+  def authenticate_user_from_token!
+    auth_token = request.headers[:authorization]
+
+    if auth_token
+
+    else
+
+    end
+  end
+
+  private
+
+  def authenticate_with_token(token)
+    unless token.include?(':')
+      authentication_error
+      return
+    end
+    user_id = token.split(':').first
+    user = User.find(user_id)
+
+    if user && Devise.secure_compare(user.access_token, token)
+      sign_in user, store: false
+    else
+      authentication_error
+    end
+  end
+
+  def authentication_error
+    render json: { error: t('errors.unauthorized') }, status: 401
+  end
 end
