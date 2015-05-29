e4d86be1789775a2079374ade9a1bf518b24d6be
Murad Yusufov <flood4life@gmail.com>
2015-05-29 13:09:45 +0300

    Refactor login params

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,13 +1,14 @@
-class LoginController < ApplicationController
+class SessionController < ApplicationController
   skip_before_action :authenticate_user_from_token!
 
   def create
-    @user = User.find_for_database_authentication(email: params[:email])
+    login_params; return if performed?
+    @user = User.find_for_database_authentication(email: login_params[:email])
     if @user.nil?
       invalid_login
       return
     end
-    if @user.valid_password?(params[:password])
+    if @user.valid_password?(login_params[:password])
       sign_in :user, @user
       render json: @user, serializer: SessionSerializer, root: nil
     else
@@ -20,6 +21,12 @@ class LoginController < ApplicationController
   def invalid_login
     warden.custom_failure!
     render json: { error: t('errors.invalid_login') }, status: :unprocessable_entity
+  end
+
+  def login_params
+    params.require(:session).permit(:email, :password)
+  rescue
+    invalid_login
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, only: []
 
   scope defaults: { format: :json } do
-    resources :login, only: :create
+    resources :login, only: :create, controller: :session
     resources :users
     resources :rules
   end
